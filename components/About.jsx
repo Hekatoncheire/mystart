7e0c54c423e5542cc3163b86c861396e34dc6a73
GyureszBa <gyorgy.dezsenyi@gmail.com>
2023-05-09 11:07:58 +0200

    main section design

--- components/About.jsx
+++ components/About.jsx
@@ -3,17 +3,17 @@ import Image from 'next/image'
 
 const About = () => {
     return (
-        <div id='about' className='flex justify-center items-center mt-20'>
-            <div className='basis-1/2 mr-20 flex-col justify-start'>
-                <h1 className='text-4xl font-bold mb-20 sm:text-7xl'>A művészet a valóság tükre - segítünk megtalálni a saját tükörképed</h1>
-                <button className='border border-black p-5'>Nézd meg aukcióinkat</button>
+        <div id='main-section' className='bg-black flex flex-col justify-center items-center text-white px-10 lg:flex-row max-lg:px-0 lg:pr-0'>
+            <div className='basis-1/2 mr-20 flex-col max-lg:mr-0 max-lg:text-center max-lg:px-10'>
+                <h1 className='text-4xl font-bold mb-20 sm:text-7xl mt-20'>A művészet a valóság tükre - segítünk neked megtalálni a saját tükörképed!</h1>
+                <button className='border border-white p-5 max-lg:mb-10'>Nézd meg aukcióinkat</button>
             </div>
             <div className='items-center justify center'>
                 <Image
                     alt="woman in art gallery"
                     src="https://images.unsplash.com/photo-1582555172866-f73bb12a2ab3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0JTIwZ2FsbGVyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    width={600}
-                    height={600}
+                    width={912}
+                    height={912}
                 />
             </div>
         </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 
 const Hero = ({ heading, buttonText }) => {
     return (
-        <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+        <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
             {/* Overlay */}
             <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[2]' />
             <div className='flex flex-col justify-center items-center p-5 text-white z-[2] mt-[-10rem]'>
                 <h2 className='text-5xl font-bold pt-40 pb-10 flex'>{heading}</h2>
-                <Link href='#about'>
+                <Link href='#main-section'>
                     <button className='px-8 py-2 border w-80 mt-10 basis-4/5 sm:basis-1/5'>{buttonText}</button>
                 </Link>
             </div>
